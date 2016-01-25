e42377129c58209aa4df360b5a505cc76484d691
Alex <alexis1784@gmail.com>
2016-01-25 13:07:38 +0500

    before migration

--- EF_lesson_4_3/Program.cs
+++ EF_lesson_4_3/Program.cs
@@ -12,9 +12,25 @@ namespace EF_lesson_4_3
         {
             using (PhoneContext db = new PhoneContext())
             {
-                var phones = db.Phones.Where(p => p.Company.Name == "Samsung");
-                foreach (Phone p in phones)
-                    Console.WriteLine("{0}.{1} - {2}", p.Id, p.Name, p.Price);
+ 
+                var phones = db.Phones.Join(db.Companies, // второй набор
+                    p => p.CompanyId, // свойство-селектор объекта из первого набора
+                    c => c.Id, // свойство-селектор объекта из второго набора
+                    (p, c) => new // результат
+                    {
+                        Name = p.Name,
+                        Company = c.Name,
+                        Price = p.Price
+                    });
+                foreach (var p in phones)
+                    Console.WriteLine("{0} ({1}) - {2}", p.Name, p.Company, p.Price);
+
+                Console.WriteLine("phones2:");
+                var phones2 = from p in db.Phones
+                             join c in db.Companies on p.CompanyId equals c.Id
+                             select new { Name = p.Name, Company = c.Name, Price = p.Price };
+                foreach (var p in phones)
+                    Console.WriteLine("{0} ({1}) - {2}", p.Name, p.Company, p.Price);
             }
             Console.ReadLine();
         }
